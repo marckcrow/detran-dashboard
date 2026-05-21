2c8ee27f2f338815655a00e2d5dbe129cc8d7e66
Marcondes <marcondesjr.ti@gmail.com>
2026-05-21 10:51:21 -0300

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    assetsDir: 'assets',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
